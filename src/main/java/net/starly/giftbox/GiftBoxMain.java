121c39b68048294e3efae533377a57da72d2db7b
yejunho10 <hyjcompany30@naver.com>
2023-05-14 10:55:14 +0900

    refactor: style code

--- src/main/java/net/starly/giftbox/GiftBoxMain.java
+++ src/main/java/net/starly/giftbox/GiftBoxMain.java
@@ -6,54 +6,52 @@ import net.starly.giftbox.command.tabcomplete.GiftBoxTab;
 import net.starly.giftbox.command.GiftBoxCmd;
 import net.starly.giftbox.listener.InventoryClickListener;
 import net.starly.giftbox.listener.InventoryCloseListener;
-import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GiftBoxMain extends JavaPlugin {
-    private static JavaPlugin plugin;
+    private static JavaPlugin instance;
     public static Config config;
 
     @Override
     public void onEnable() {
-        // DEPENDENCY
-        if (!isPluginEnabled("net.starly.core.StarlyCore")) {
+        /* DEPENDENCY
+         ──────────────────────────────────────────────────────────────────────────────────────────────────────────────── */
+        if (!isPluginEnabled("ST-Core")) {
             getServer().getLogger().warning("[" + getName() + "] ST-Core 플러그인이 적용되지 않았습니다! 플러그인을 비활성화합니다.");
             getServer().getLogger().warning("[" + getName() + "] 다운로드 링크 : §fhttp://starly.kr/");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
+        /* SETUP
+         ──────────────────────────────────────────────────────────────────────────────────────────────────────────────── */
+        instance = this;
+        new Metrics(instance, 17440);
 
-        plugin = this;
-        new Metrics(plugin, 17440);
-
-
-        // CONFIG
-        config = new Config("config", plugin);
+        /* CONFIG
+         ──────────────────────────────────────────────────────────────────────────────────────────────────────────────── */
+        config = new Config("config", instance);
         config.loadDefaultConfig();
         config.setPrefix("messages.prefix");
 
-
-        // COMMANDS
+        /* COMMAND
+         ──────────────────────────────────────────────────────────────────────────────────────────────────────────────── */
         getServer().getPluginCommand("giftbox").setExecutor(new GiftBoxCmd());
         getServer().getPluginCommand("giftbox").setTabCompleter(new GiftBoxTab());
 
-
-        // EVENTS
-        getServer().getPluginManager().registerEvents(new InventoryClickListener(), plugin);
-        getServer().getPluginManager().registerEvents(new InventoryCloseListener(), plugin);
+        /* LISTENER
+         ──────────────────────────────────────────────────────────────────────────────────────────────────────────────── */
+        getServer().getPluginManager().registerEvents(new InventoryClickListener(), instance);
+        getServer().getPluginManager().registerEvents(new InventoryCloseListener(), instance);
     }
 
-    public static JavaPlugin getPlugin() {
-        return plugin;
+    public static JavaPlugin getInstance() {
+        return instance;
     }
 
-    private boolean isPluginEnabled(String path) {
-        try {
-            Class.forName(path);
-            return true;
-        } catch (ClassNotFoundException ignored) {
-        } catch (Exception ex) { ex.printStackTrace(); }
-        return false;
+    private boolean isPluginEnabled(String name) {
+        Plugin plugin = getServer().getPluginManager().getPlugin(name);
+        return plugin != null && plugin.isEnabled();
     }
 }
