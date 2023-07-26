6c3da4d70e0eb0dc3f53dd02927b8a4e00c370c8
yejunho10 <hyjcompany30@naver.com>
2023-07-26 15:32:11 +0900

    refactor: style code fix: bugs

--- src/main/java/net/starly/giftbox/listener/InventoryClickListener.java
+++ src/main/java/net/starly/giftbox/listener/InventoryClickListener.java
@@ -17,67 +17,55 @@ import static net.starly.giftbox.data.InventoryOpenMap.inventoryOpenMap;
 
 public class InventoryClickListener implements Listener {
     @EventHandler
-    public void onClick(InventoryClickEvent e) {
-        Player p = (Player) e.getWhoClicked();
-        if (!inventoryOpenMap.containsKey(p)) return;
+    public void onClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        if (!inventoryOpenMap.containsKey(player.getUniqueId())) return;
 
-        if (e.getAction() == InventoryAction.HOTBAR_MOVE_AND_READD || e.getAction() == InventoryAction.HOTBAR_SWAP) {
-            e.setCancelled(true);
+        Inventory inventory = event.getClickedInventory();
+        if (inventory == player.getInventory()) {
+            event.setCancelled(true);
             return;
         }
 
-        if (e.getClickedInventory() == p.getInventory()) {
-            e.setCancelled(true);
-            return;
-        }
+        ItemStack currentItem = event.getCurrentItem();
+        if (currentItem == null) return;
+        event.setCancelled(true);
 
-        ItemStack item = e.getCurrentItem();
-        if (item == null) return;
+        int slot = event.getSlot();
+        if (slot == 49) {
+            event.setCancelled(true);
 
-        int slotNum = e.getSlot();
-        if (slotNum == 49) {
-            // 모두수령 클릭
-
-            PlayerGiftBoxData playerGiftBoxData = new PlayerGiftBoxData(inventoryOpenMap.get(p));
-
-            e.setCancelled(true);
-
-            p.closeInventory();
+            PlayerGiftBoxData playerGiftBoxData = new PlayerGiftBoxData(inventoryOpenMap.get(player.getUniqueId()));
+            player.closeInventory();
 
             if (playerGiftBoxData.getItems().size() == 0) {
-                p.sendMessage(config.getMessage("messages.empty"));
+                player.sendMessage(config.getMessage("messages.empty"));
                 return;
-            } else if (InventoryUtil.getSpace(p.getInventory()) - 5 < playerGiftBoxData.getItems().size()) {
-                p.sendMessage(config.getMessage("messages.cannot_receive"));
+            } else if (InventoryUtil.getSpace(player.getInventory()) - 5 < playerGiftBoxData.getItems().size()) {
+                player.sendMessage(config.getMessage("messages.cannot_receive"));
                 return;
             }
 
-            Inventory inv = p.getInventory();
+            Inventory inv = player.getInventory();
             playerGiftBoxData.getItems().forEach(inv::addItem);
             playerGiftBoxData.clearItems();
 
-            p.sendMessage(config.getMessage("messages.all_received"));
-        } else if (Arrays.asList(45, 46, 47, 48, 50, 51, 52, 53).contains(slotNum)) {
-            // 빈칸 아이템 클릭
-
-            e.setCancelled(true);
-        } else {
-            // 일반 아이템 클릭
-
-            if (e.getClickedInventory() == p.getInventory()) return;
-            if (InventoryUtil.getSpace(p.getInventory()) - 5 < 1) {
-                p.sendMessage(config.getMessage("messages.cannot_receive"));
+            player.sendMessage(config.getMessage("messages.all_received"));
+        } else if (!(slot >= 45 && slot <= 53)) {
+            if (event.getClickedInventory() == player.getInventory()) return;
+            if (InventoryUtil.getSpace(player.getInventory()) - 5 < 1) {
+                player.sendMessage(config.getMessage("messages.cannot_receive"));
                 return;
             }
 
-            PlayerGiftBoxData playerGiftBoxData = new PlayerGiftBoxData(inventoryOpenMap.get(p));
-            System.out.println(inventoryOpenMap.get(p));
+            PlayerGiftBoxData playerGiftBoxData = new PlayerGiftBoxData(inventoryOpenMap.get(player.getUniqueId()));
+            System.out.println(inventoryOpenMap.get(player.getUniqueId()));
 
-            p.getInventory().addItem(item);
-            playerGiftBoxData.removeItem(item);
+            player.getInventory().addItem(currentItem);
+            playerGiftBoxData.removeItem(currentItem);
 
-            p.closeInventory();
-            p.sendMessage(config.getMessage("messages.receipted"));
+            player.closeInventory();
+            player.sendMessage(config.getMessage("messages.receipted"));
         }
     }
 }
